1745a1b2c333e770630afb8f1e9d2f68da5c7aa8
Jevirs <644658982@qq.com>
2019-05-31 17:58:05 +0800

    modify list

--- lib/main.dart
+++ lib/main.dart
@@ -20,49 +20,40 @@ class MyAppState extends State<MyApp> {
     try {
       var request = await httpClient.getUrl(Uri.parse(url));
       var response = await request.close();
-      if (response.statusCode == HttpStatus.OK) {
+      if (response.statusCode == HttpStatus.ok) {
         var json = await response.transform(utf8.decoder).join();
         var data = jsonDecode(json);
         var length = data['data'].length;
-        final _random = new Random();
-        int next(int min, int max) => min + _random.nextInt(max - min);
-        result = data['data'][next(0, length)];
+        result = data['data'][new Random().nextInt(length)];
       } else {
-        result = '没东东';
+        result = '没找到吃的';
       }
     } catch (exception) {
-      result = exception.toString();
+      result = "没网只能吃二烧了";
     }
-
-    // If the widget was removed from the tree while the message was in flight,
-    // we want to discard the reply rather than calling setState to update our
-    // non-existent appearance.
     if (!mounted) return;
-
     setState(() {
       word = result;
     });
   }
 
   @override
-  Widget build(BuildContext context) {
-    return new MaterialApp(
-      title: 'YunjiFinance',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: new Text('吃啥呢'),
+  Widget build(BuildContext context) => new MaterialApp(
+        title: 'YunjiFinance',
+        home: new Scaffold(
+          appBar: new AppBar(
+            title: new Text('吃啥呢'),
+          ),
+          body: new Center(
+            child: new Text(word),
+          ),
+          floatingActionButton: FloatingActionButton(
+            onPressed: () {
+              _refresh();
+            },
+            child: Icon(Icons.refresh),
+            backgroundColor: Colors.pink,
+          ),
         ),
-        body: new Center(
-          child: new Text(word),
-        ),
-        floatingActionButton: FloatingActionButton(
-          onPressed: () {
-            _refresh();
-          },
-          child: Icon(Icons.refresh),
-          backgroundColor: Colors.pink,
-        ),
-      ),
-    );
-  }
+      );
 }
