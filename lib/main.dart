b5e130b85c1f1472e29a7c5acb05e2d83ba2370e
Jevirs <644658982@qq.com>
2019-10-14 17:22:11 +0800

    fix: add cookie

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'dart:io';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:toast/toast.dart';
 
 void main() => runApp(MyApp());
 
@@ -7,34 +9,38 @@ class MyApp extends StatefulWidget {
   State<StatefulWidget> createState() => MyAppState();
 }
 
-String result = '???';
-TextEditingController _controller = TextEditingController(text: "2097");
-
-_didi(BuildContext context) async {
-  var _url = '';
-  var url = _url + _controller.text;
-  var httpClient = HttpClient();
-  try {
-    var request = await httpClient.getUrl(Uri.parse(url));
-    var response = await request.close();
-    if (response.statusCode == HttpStatus.ok) {
-      result = "成功了";
-    } else {
-      result = '失败了';
-    }
-  } catch (exception) {
-    result = "有网吗？";
-  }
-  _showToast(context);
-}
-
-_showToast(BuildContext context) {
-  print(result);
-  Scaffold.of(context).showSnackBar(
-      SnackBar(content: Text(result), backgroundColor: Colors.blue));
-}
+SharedPreferences prefs;
+TextEditingController _controller = TextEditingController(text: "");
+TextEditingController _controller2 = TextEditingController(text: "");
 
 class MyAppState extends State<MyApp> {
+  @override
+  void initState() {
+    super.initState();
+    _asyncInit();
+  }
+
+  void showToast(String msg, {int duration, int gravity}) {
+    Toast.show(msg, context, duration: duration, gravity: gravity);
+  }
+
+  _asyncInit() async {
+    prefs = await SharedPreferences.getInstance();
+    String code = prefs.getString("code");
+    String cookie = prefs.getString("cookie");
+
+    _controller.text = code;
+    _controller2.text = cookie;
+  }
+
+  _save() async {
+    String code = _controller.text;
+    String cookie = _controller2.text;
+    prefs.setString("code", code);
+    prefs.setString("cookie", cookie);
+    showToast("保存成功");
+  }
+
   @override
   Widget build(BuildContext context) => MaterialApp(
         title: 'DIDI',
@@ -47,7 +53,7 @@ class MyAppState extends State<MyApp> {
               child: Container(
                 child: Column(
                   children: <Widget>[
-                    Text("DIDI Helper",
+                    Text("DIDI打卡",
                         style: TextStyle(color: Colors.black, fontSize: 36)),
                     TextField(
                       controller: _controller,
@@ -55,6 +61,17 @@ class MyAppState extends State<MyApp> {
                       decoration: InputDecoration(
                         hintText: '请输入工号',
                       ),
+                    ),
+                    TextField(
+                      controller: _controller2,
+                      style: TextStyle(fontSize: 18),
+                      decoration: InputDecoration(hintText: '请输入JESSIONID值'),
+                    ),
+                    RaisedButton(
+                      onPressed: () => {_save()},
+                      child: Text("保存"),
+                      textColor: Colors.black,
+                      focusColor: Colors.red,
                     )
                   ],
                 ),
@@ -68,8 +85,38 @@ class MyAppState extends State<MyApp> {
 class FatButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    void showToast(String msg, {int duration, int gravity}) {
+      Toast.show(msg, context, duration: duration, gravity: gravity);
+    }
+
+    _didi() async {
+      String result;
+      var _url = 'http://attendance.' +
+          'yunjiglobal' +
+          '.com/attendance/attendance/check?workcode=';
+      var url = _url + _controller.text;
+      var httpClient = HttpClient();
+      try {
+        var request = await httpClient.getUrl(Uri.parse(url));
+        request.headers.add("user-agent",
+            "Mozilla/5.0 (iPhone; CPU iPhone OS 10_3_1 like Mac OS X) AppleWebKit/603.1.30 (KHTML, like Gecko) Version/10.0 Mobile/14E304 Safari/602.1");
+        request.headers.add("cookie", _controller2.text);
+        var response = await request.close();
+        print(response.toString());
+        if (response.statusCode == HttpStatus.ok) {
+          result = "成功了";
+        } else {
+          result = '失败了';
+        }
+      } catch (exception) {
+        result = "有网吗？";
+      }
+      httpClient.close();
+      showToast(result);
+    }
+
     return FloatingActionButton(
-      onPressed: () => {_didi(context)},
+      onPressed: () => {_didi()},
       child: Icon(Icons.check),
       backgroundColor: Colors.blue,
     );
